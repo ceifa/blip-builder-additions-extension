6a25b801686e09b9fcca27aff07ad5ff01288385
Gabriel Santos <gabriels@take.net>
2019-01-16 18:23:40 -0200

    Add chatbase input icon

--- js/features/chatbase.js
+++ js/features/chatbase.js
@@ -4,6 +4,10 @@ const chatbaseMessageIcon = `
     <img style="margin-top: -35px; margin-right: -2px;" src="https://i.imgur.com/JKnS5xi.png" />
 `;
 
+const chatbaseInputIcon = `
+    <img src="https://i.imgur.com/JKnS5xi.png" style="width: 25px;height: 100%;">
+`;
+
 const chatbaseMessageModal = `
     <div class="modal-overlay chatbase-close-modal"></div>
     <form class="modal-dialog modal-sm layout-center-y">
@@ -14,12 +18,17 @@ const chatbaseMessageModal = `
         </div>
 
         <div class="modal-body row">
-            <h1 class="mb5 mt4-1" >Restore published version</h1>
-            <div class="mh6 ph4" >If you restore the last published version, you may loose any changes made in the flow since then. Do you want to proceed?</div>
+            <h1 class="mb5 mt4-1" >Insert chatbase bot response</h1>
+            <div class="mh6 ph4" style="text-align: start">
+                <div class="mt3 labeled-input">
+                    <label>Intention</label>
+                    <input type="text" />
+                </div>
+            </div>
         </div>
 
         <div class="modal-footer">
-            <button type="submit" class="button-primary w-100">Yes</button>
+            <button type="submit" class="button-primary w-100">Confirm</button>
         </div>
     </form>
 `;
@@ -35,6 +44,8 @@ function stopChatbase() {
 hook.add("new-element", (el) => {
     if (chatbase_started && el.className && el.className.includes("anchor-container") && el.className.includes("component-card-container")) {
         addChatbaseMessageOptions(el);
+    } else if (chatbase_started && el.id === "node-content-tab") {
+        addChatbaseInputOption(el);
     }
 });
 
@@ -48,7 +59,13 @@ function addChatbaseMessageOptions(message) {
     const leftBubble = message.querySelector(".bubble.left");
     const isTypingOrHasIconOrAreEditing = leftBubble && Array.from(leftBubble.children).some(c => c.className.includes("typing") || c.className.includes("chatbaseIco") || c.tagName.toLowerCase() === "form");
 
-    if (!isTypingOrHasIconOrAreEditing) {
+    if (leftBubble && !isTypingOrHasIconOrAreEditing) {
+        if (true) {
+            const noChatbasePoint = document.createElement('div');
+            noChatbasePoint.style = "background-color: #f76556;width: 10px;height: 10px;border-radius: 50%;margin-left: auto;margin-right: 25px;";
+            leftBubble.parentElement.append(noChatbasePoint);
+        }
+
         const chatbaseOption = document.createElement("div");
         chatbaseOption.style = "right: 51px; background-color: gold;";
         chatbaseOption.className = "editIco chatbaseIco";
@@ -64,9 +81,24 @@ function addChatbaseMessageOptions(message) {
             for (const closer of closers) {
                 closer.onclick = function() {
                     document.getElementById("chatbase-modal").remove();
-                }
+                };
             }
         };
         leftBubble.append(chatbaseOption);
     }
+}
+
+function addChatbaseInputOption(el) {
+    const tabItems = document.getElementsByClassName("content-tabs-items")[0];
+
+    if (tabItems) {
+        const chatbaseOption = document.createElement("li");
+        chatbaseOption.style = "padding: 0;margin-left: 8px;display: inline-flex;vertical-align: middle;cursor: pointer;";
+        chatbaseOption.innerHTML = chatbaseInputIcon;
+        tabItems.append(chatbaseOption);
+    }
+}
+
+function getToast() {
+    return angular.element(document.body).injector().get('ngToast');
 }
