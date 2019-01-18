ab031d98c9936cf3c5a460cd861d8166342698cc
Gabriel Santos <gabriels@take.net>
2019-01-18 07:47:13 -0200

    add process command tag

--- js/shared/shared.js
+++ js/shared/shared.js
@@ -11,28 +11,32 @@ const defaultSettings = {
     'redirect-to-service-color': '#1EA1FF',
     'set-contact-color': '#FF1E90',
     'process-http-color': '#7762E3',
-    'set-variable-color': '#FF4A1E'
+    'set-variable-color': '#FF4A1E',
+    'process-command-color': '#FC91AE'
 };
 
 const possibleActions = [{
-        name: 'Event tracking',
-        alias: [ 'Registro de eventos', 'TrackEvent' ]
-    },{
-        name: 'Execute script',
-        alias: [ 'Executar script', 'ExecuteScript' ]
-    },{
-        name: 'Manage distribution list',
-        alias: [ 'Gerenciar lista de distribuição', 'ManageList' ]
-    },{
-        name: 'Redirect to service',
-        alias: [ 'Redirecionar a um serviço', 'Redirect' ]
-    },{
-        name: 'Set contact',
-        alias: [ 'Definir contato', 'MergeContact' ]
-    },{
-        name: 'Process HTTP',
-        alias: [ 'Requisição HTTP', 'ProcessHttp' ]
-    },{
-        name: 'Set variable',
-        alias: [ 'Definir variável', 'SetVariable' ]
-    }];
+    name: 'Event tracking',
+    alias: ['Registro de eventos', 'TrackEvent']
+}, {
+    name: 'Execute script',
+    alias: ['Executar script', 'ExecuteScript']
+}, {
+    name: 'Manage distribution list',
+    alias: ['Gerenciar lista de distribuição', 'ManageList']
+}, {
+    name: 'Redirect to service',
+    alias: ['Redirecionar a um serviço', 'Redirect']
+}, {
+    name: 'Set contact',
+    alias: ['Definir contato', 'MergeContact']
+}, {
+    name: 'Process HTTP',
+    alias: ['Requisição HTTP', 'ProcessHttp']
+}, {
+    name: 'Set variable',
+    alias: ['Definir variável', 'SetVariable']
+}, {
+    name: 'Process command',
+    alias: ['Processar comando', 'ProcessCommand']
+}];
