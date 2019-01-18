b11719b5276cef72d8e07074cbde3f4de79cf6fa
Gabriel Santos <gabriels@take.net>
2019-01-18 17:10:34 -0200

    add chatbase bot message to edit

--- js/features/chatbase.js
+++ js/features/chatbase.js
@@ -24,6 +24,10 @@ const chatbaseMessageModal = `
                     <label>Intention</label>
                     <input type="text" />
                 </div>
+                <div class="mt3 labeled-input">
+                    <label>Bot response message</label>
+                    <input id="bot-response-message" type="text" />
+                </div>
             </div>
         </div>
 
@@ -70,12 +74,16 @@ function addChatbaseMessageOptions(message) {
         chatbaseOption.style = "right: 51px; background-color: gold;";
         chatbaseOption.className = "editIco chatbaseIco";
         chatbaseOption.innerHTML = chatbaseMessageIcon;
+        chatbaseOption.botResponseMessage = leftBubble.lastElementChild.textContent.trim();
         chatbaseOption.onclick = function() {
             const modal = document.createElement("div");
             modal.id = "chatbase-modal";
             modal.className = "modal";
             modal.innerHTML = chatbaseMessageModal;
             document.body.append(modal);
+
+            const botMessageElement = document.getElementById("bot-response-message");
+            botMessageElement.value = this.botResponseMessage;
 
             const closers = document.getElementsByClassName("chatbase-close-modal");
             for (const closer of closers) {
@@ -99,6 +107,10 @@ function addChatbaseInputOption(el) {
     }
 }
 
+function getSelectedNode() {
+    return document.querySelector('.selected-node.editing-node');
+}
+
 function getToast() {
-    return angular.element(document.body).injector().get('ngToast');
+    return angular && angular.element(document.body).injector().get('ngToast');
 }
